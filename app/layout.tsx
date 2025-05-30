641d6931e4467de7909f0bbcddf84caf1f4831d7
mahmudx <mahmudnotes@outlook.com>
2025-05-30 18:14:41 +0600

    Initial commit 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next'
 import './globals.css'
-
+import Clarity from '@microsoft/clarity';
+import Script from "next/script";
 export const metadata: Metadata = {
   title: 'হৃদয়ে নাম্বারস্কিলস',
   description: "Team Numberskills' desk Occupancy and Memories"
 }
-
+Clarity.init('rrn9qbf9m9');
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,6 +14,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="microsoft-clarity-analytics">
+  {`
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "rrn9qbf9m9");
+  `}
+</Script>
       <body>{children}</body>
     </html>
   )
